112ea8ddff4e46ef8a5254988d56345a65ee38e5
a.nurmagambetov <a.nurmagambetov@yandex.kz>
2023-03-16 10:48:32 +0600

    step10

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Content = () => {
     return (
         <div className='content'>
-            Main Cont 10:36
+            Main Cont 10:48
         </div>
     )
 }
