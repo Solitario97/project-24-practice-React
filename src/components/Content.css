fc6314391a5cf9ec4262db27c56edd1a3d1ae88f
a.nurmagambetov <a.nurmagambetov@yandex.kz>
2023-03-16 10:29:53 +0600

    step 4

---
+++ src/components/Content.css
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Content = () => {
+    return (
+        <div className='content'>
+            Main content
+        </div>
+    )
+}
+
+export default Content;
