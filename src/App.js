c9324e79d8c36fe223790b4f85da668be974f6a2
Solitario97 <a.nurmagambetov97@gmail.com>
2023-03-18 15:52:52 +0600

    road to the dream v3

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
-const App = () => {
+const App =() => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='main-content'>
-        <Profile />
-       {/*  <Dialogs/> */}
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header/>
+        <Navbar/>
+        <div className='main-content'>
+          <Routes>
+            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/dialogs' element={<Dialogs/>}/>
+          </Routes>
+        </div>
       </div>
-    </div>
-  );
+    </BrowserRouter>
+  )
 }
-
 
 
 export default App;
