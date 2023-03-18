8967ade67f1467d4a2e50cfba2c96340acc2b1ad
Solitario97 <a.nurmagambetov97@gmail.com>
2023-03-18 10:32:14 +0600

    first work with props v2

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from "./Dialogs.module.css";
+
+
+const Dialogs = () => {
+    return (
+        <div className={s.title}>
+            Dialogs
+        </div>
+    )
+}
+
+export default Dialogs; 
