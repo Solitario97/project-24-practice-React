84953418becd6f451cc1a6cd65c9420e30c67ae3
Solitario97 <a.nurmagambetov97@gmail.com>
2023-03-19 07:59:00 +0600

    new day new lessons v3

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,38 +1,39 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
 
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
-            <div className="dialogs-item">
-                <div className="dialog">
-                    Amir
+            <div className={s.dialogsItem}>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/1'>Amir</NavLink>
                 </div>
-                <div className="dialog">
-                    Dima
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'>Dima</NavLink>
                 </div>
-                <div className="dialog">
-                    Albina
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3'>Anuar</NavLink>
                 </div>
-                <div className="dialog">
-                    Mira
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4'>Albina</NavLink>
                 </div>
-                <div className="dialog">
-                    Mom
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/5'>Wakanda</NavLink>
                 </div>
-                <div className="dialog">
-                    Anuar
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/6'>Alexander</NavLink>
                 </div>
-                <div className="dialog">
-                    Alya
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/7'>Alya</NavLink>
                 </div>
             </div>
-            <div className="messages">
-                <div className="message">How are u?</div>
-                <div className="message">do u know?</div>
-                <div className="message">i love Turkey</div>
-                <div className="message">Yes, u right</div>
+            <div className={s.messages}>
+                <div className={s.message}>How are u?</div>
+                <div className={s.message}>do u know?</div>
+                <div className={s.message}>i love Turkey</div>
+                <div className={s.message}>Yes, u right</div>
             </div>
         </div>
     )
