3940f03d55e6899106e2f9e5061b9b8d515283d8
Solitario97 <a.nurmagambetov97@gmail.com>
2023-03-19 20:43:34 +0600

    new day new lessons v6

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -21,22 +21,37 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let DialogData = [
+        {name:"Amir", id:'1'},
+        {name:"Imran", id:'2'},
+        {name:"Altair", id:'3'},
+        {name:"Ecio", id:'4'},
+        {name:"Franchesko", id:'5'},
+    ]
+
+    let MessageData = [
+        {message:'How are u?', id:'1'},
+        {message:'Amir your two safes are full of money, where to put the money now', id:'2'},
+        {message:'Have you already bought a Porsche or are you just going to get it ?', id:'3'},
+        {message:"Let's fly to France for a couple of days on the way back", id:'4'},
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
-                <DialogItem name="Amir" id='1'/>
-                <DialogItem name="Imran" id='2'/>
-                <DialogItem name="Altair" id='3'/>
-                <DialogItem name="Ecio" id='4'/>
-                <DialogItem name="Franchesko" id='5'/>
-                <DialogItem name="Albina" id='6'/>
-                <DialogItem name="German" id='7'/>
+                <DialogItem name={DialogData[0].name} id={DialogData[0].id} />
+                <DialogItem name={DialogData[1].name} id={DialogData[1].id} />
+                <DialogItem name={DialogData[2].name} id={DialogData[2].id} />
+                <DialogItem name={DialogData[3].name} id={DialogData[3].id} />
+                <DialogItem name={DialogData[4].name} id={DialogData[4].id} />
             </div>
+            
             <div className={s.messages}>
-                <Message message='How are u?'/>
-                <Message message='Amir your two safes are full of money, where to put the money now'/>
-                <Message message='Have you already bought a Porsche or are you just going to get it ?'/>
-                <Message message="Let's fly to France for a couple of days on the way back"/>
+                <Message message={MessageData[0].message} id={MessageData[0].id}/>
+                <Message message={MessageData[1].message} id={MessageData[1].id}/>
+                <Message message={MessageData[2].message} id={MessageData[2].id}/>
+                <Message message={MessageData[3].message} id={MessageData[3].id}/>
             </div>
         </div>
     )
