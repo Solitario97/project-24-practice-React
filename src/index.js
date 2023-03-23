65faddfa3cbe8110273c4e314aa369d974f426e2
Solitario97 <a.nurmagambetov97@gmail.com>
2023-03-23 10:27:04 +0600

    again again again

--- src/index.js
+++ src/index.js
@@ -3,37 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from './redux/state';
 
-let PostsData = [
-  {message: 'Hello, my name is Amir', count: '10'},
-  {message: 'My first work with props', count: '300'},
-  {message: 'several day`s ago i bought macbook', count: '1000'},
-];
-
-let DialogData = [
-  {name:"Amir", id:'1'},
-  {name:"Imran", id:'2'},
-  {name:"Altair", id:'3'},
-  {name:"Ecio", id:'4'},
-  {name:"Franchesko", id:'5'},
-]
-
-let MessageData = [
-  {message:'How are u?', id:'1'},
-  {message:'Amir your two safes are full of money, where to put the money now', id:'2'},
-  {message:'Have you already bought a Porsche or are you just going to get it ?', id:'3'},
-  {message:"Let's fly to France for a couple of days on the way back", id:'4'},
-]
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App 
-    PostsData={PostsData} 
-    DialogData={DialogData} 
-    MessageData={MessageData}
-    />
+    <App state={state}/>
   </React.StrictMode>
 );
 
---
+++ src/redux/state.js
@@ -0,0 +1,26 @@
+let state = {
+
+    profilePage : {
+        PostsData: [
+            {message: 'Hello, my name is Amir', count: '10'},
+            {message: 'My first work with props', count: '300'},
+            {message: 'several day`s ago i bought macbook', count: '1000'},
+          ],
+    },
+
+    messagePage : {
+        DialogData: [
+            {name:"Amir", id:'1'},
+            {name:"Imran", id:'2'},
+            {name:"Altair", id:'3'},
+            {name:"Ecio", id:'4'},
+            {name:"Franchesko", id:'5'},
+          ],
+        MessageData: [
+            {message:'How are u?', id:'1'},
+            {message:'Amir your two safes are full of money, where to put the money now', id:'2'},
+            {message:'Have you already bought a Porsche or are you just going to get it ?', id:'3'},
+            {message:"Let's fly to France for a couple of days on the way back", id:'4'},
+          ],
+    },
+}
