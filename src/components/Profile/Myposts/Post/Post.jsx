2dcf6665afd011beb9cd9f7f5a11c5a28febdb99
Solitario97 <a.nurmagambetov97@gmail.com>
2023-03-24 06:40:27 +0600

    hello !

--- src/components/Profile/Myposts/Post/Post.jsx
+++ src/components/Profile/Myposts/Post/Post.jsx
@@ -5,6 +5,7 @@ import c from './Post.module.css';
 const Post = (props) => {
     return (
         <div className={c.item}>
+            <img src="./fox.jpg" alt="fox" />
             {props.message}
             <div>
                 {" "}like: {props.count} 
