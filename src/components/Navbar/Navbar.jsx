2a7833d0e5c7adb420c30a17bc05a03e84c29f8a
Solitario97 <a.nurmagambetov97@gmail.com>
2023-03-25 10:29:15 +0600

    well well well

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,9 +27,9 @@ const Navbar = () => {
                 </NavLink>
             </div>
             <div className={n.friends}>
-                <a href="#" className={n.friend__item}>2</a>
-                <a href="#" className={n.friend__item}>2</a>
-                <a href="#" className={n.friend__item}>2</a>
+                <a href="#" className={n.friend__items}>2</a>
+                <a href="#" className={n.friend__items}>2</a>
+                <a href="#" className={n.friend__items}>2</a>
             </div>
         </nav>
     )
