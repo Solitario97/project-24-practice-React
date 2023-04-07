81b78976c54c75c2b4c34c70b264bec88c2b29ca
Solitario97 <a.nurmagambetov97@gmail.com>
2023-04-07 09:30:14 +0600

    abra cadabra

--- src/components/Profile/Myposts/Post/Post.jsx
+++ src/components/Profile/Myposts/Post/Post.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import c from './Post.module.css';
 
-
 const Post = (props) => {
     return (
         <div className={c.item}>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,6 +1,4 @@
-let Rerender = () => {
-  console.log('change')
-}
+
 
 let state = {
 
