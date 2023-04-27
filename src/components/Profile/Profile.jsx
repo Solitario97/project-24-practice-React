03870d263462674108aa6aeb17934cab418ef848
Solitario97 <a.nurmagambetov97@gmail.com>
2023-04-27 19:02:47 +0600

    asd

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import c from './Profile.module.css';
 import Mypost from './Myposts/Mypost';
 
+
 const Profile = (props) => {
     return (
         <div className={c.Profile}>
