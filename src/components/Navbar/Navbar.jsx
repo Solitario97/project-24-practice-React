f70c119a8e4c0c02829a3bad0620e4a835d1f64b
Solitario97 <a.nurmagambetov97@gmail.com>
2023-05-06 10:00:00 +0600

    Добавлены изменения в index.html

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import n from './Navbar.module.css';
 
+
 const Navbar = () => {
     return (
         <nav className={n.nav}>
